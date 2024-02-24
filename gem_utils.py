f6afb80acbd819868162672d628cfc511d6117e7
jchavezar <jchavezar@gmail.com>
2024-02-24 00:50:17 -0500

    feb-24-24

--- gem_utils.py
+++ gem_utils.py
@@ -1,10 +1,11 @@
-LOCATION = "us-central1"
-PROJECT_ID = 'vtxdemos' # PUT YOUR PROJECT HERE!
-
 from google.cloud import aiplatform
 from vertexai.preview.generative_models import GenerationResponse
 from proto.marshal.collections import repeated
 from proto.marshal.collections import maps
+
+LOCATION = "us-central1"
+PROJECT_ID = 'vtxdemos'  # PUT YOUR PROJECT HERE!
+
 aiplatform.init(project=PROJECT_ID, location=LOCATION)
 
 
@@ -23,6 +24,7 @@ def recurse_proto_repeated_composite(repeated_object):
 
     return repeated_list
 
+
 def recurse_proto_marshal_to_dict(marshal_object):
     new_dict = {}
     for k, v in marshal_object.items():
@@ -38,9 +40,11 @@ def recurse_proto_marshal_to_dict(marshal_object):
 
     return new_dict
 
+
 def get_text(response: GenerationResponse):
     """Returns the Text from the Generation Response object."""
     part = response.candidates[0].content.parts[0]
+    # noinspection PyBroadException
     try:
         text = part.text
     except:
@@ -48,9 +52,10 @@ def get_text(response: GenerationResponse):
 
     return text
 
+
 def get_function_name(response: GenerationResponse):
     return response.candidates[0].content.parts[0].function_call.name
 
+
 def get_function_args(response: GenerationResponse) -> dict:
     return recurse_proto_marshal_to_dict(response.candidates[0].content.parts[0].function_call.args)
-
