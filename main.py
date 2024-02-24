c0d6a22b52d295cf5e5d6fd916ca228122d457ce
jchavezar <jchavezar@gmail.com>
2024-02-24 00:39:33 -0500

    feb-24-24

---
+++ main.py
@@ -0,0 +1,213 @@
+import json
+import vertexai
+import gem_utils
+import streamlit as st
+from google.cloud import storage
+import vertexai.preview.generative_models as generative_models
+from vertexai.preview.generative_models import GenerativeModel, Tool
+
+project_id = "vtxdemos"
+bucket_id = "vtxdemos-nbademos"
+
+client = storage.Client(project_id)
+bucket = client.bucket(bucket_id)
+vertexai.init(project="vtxdemos", location="us-central1")
+model = GenerativeModel("gemini-1.0-pro-001")
+
+# %%
+# Getting tables from stats
+# gemini-pro functions
+get_stats_funct = {
+    "name": "get_stats_funct",
+    "description": "getting nba statistics from teams",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "team_1": {
+                "type": "string",
+                "description": "an nba team"
+            },
+            "team_2": {
+                "type": "string",
+                "description": "an nba team"
+            },
+        },
+        "required": [
+            "team_1",
+            "team_2"
+        ]
+    }
+}
+
+all_tools = Tool.from_dict(
+    {
+        "function_declarations": [
+            get_stats_funct,
+        ]
+    }
+)
+
+
+def get_stats_funct(team_1: str, team_2: str):
+    import requests
+    from bs4 import BeautifulSoup
+
+    utils_dictionary = {
+        "Boston Celtics": "celtics",
+        "Cleveland Cavaliers": "cavaliers",
+        "Denver Nuggets": "nuggets",
+        "Brooklyn Nets": "nets",
+        "Charlotte Hornets": "hornets",
+        "Chicago Bulls": "bulls",
+        "Detroit Pistons": "pistons",
+        "Indiana Pacers": "pacers",
+        "Minnesota Timberwolves": "timberwolves",
+        "Atlanta Hawks": "hawks",
+        "Milwaukee Bucks": "bucks",
+        "New York Knicks": "knicks",
+        "Golden State Warriors": "warriors",
+        "Dallas Mavericks": "mavericks",
+        "L a clippers": "clippers",
+        "Houston Rockets": "rockets",
+        "Los Angeles Lakers": "lakers",
+        "Miami Heat": "heat",
+        "Philadelphia 76ers": "76ers",
+        "Memphis Grizzlies": "grizzlies",
+        "New Orleans Pelicans": "pelicans",
+        "Oklahoma City Thunder": "thunder",
+        "Portland Trail Blazers": "blazers",
+        "Toronto Raptors": "raptors",
+    }
+
+    responses = model.generate_content(f"""
+    You are a NBA analyst, your task is to get the intent/name of the `Teams` and extract the pseudonym `Value` 
+    from the dictionary below:
+    
+    Dictionary
+    str{utils_dictionary}
+    
+    ###Example###
+    Teams:
+    team_1: Chicago super bulls
+    team_2: New York Sun Nicks
+    
+    Output: bulls, nicks
+    ###End of Example###
+    
+    Teams:
+    team_1: {team_1}
+    team_2: {team_2}
+    
+    Output:
+    """)
+    team_1, team_2 = responses.text.split(",")
+
+    url = f"https://www.statmuse.com/nba/ask/{team_1}-vs-{team_2}-stats-last-5-games"
+    req = requests.get(url)
+    sp = BeautifulSoup(req.content, 'lxml')
+    table = sp.find("table")
+    table_body = table.find("tbody")
+    rows = table_body.find_all("tr")
+    data_from_table = []
+
+    # Cleaning Data
+    for row in rows:
+        cols = row.find_all("td")
+        cols = [ele.text.strip() for ele in cols]
+        data_from_table.append([ele for ele in cols if ele])
+
+    stats = {}
+    for num, text in enumerate(data_from_table):
+        stats[f"game_on_date_{text[1]}"] = {
+            "team": text[0],
+            "date": text[1],
+            "match": " ".join(text[2:5]),
+            "result": text[5],
+            "min": text[6],
+            "points": text[7],
+            "reb": text[8],
+            "assist": text[9],
+            "steals": text[10],
+            "blocked_shots": text[11],
+            "turnovers": text[12],
+            "field_goals_made": text[13],
+            "field_goals_attempted": text[14],
+            "field_goals_%": text[15],
+            "3_point_field_goals_made": text[16],
+            "3_point_field_goals_attempted": text[17],
+            "3_point_field_goals_attempted_%": text[18],
+            "free_throws_made": text[19],
+            "free_throws_attempted": text[20],
+            "free_throws_%": text[21],
+            "power_forward": text[22],
+        }
+        st.write(stats)
+    return str(stats)
+
+
+data = get_stats_funct("celtics", "76ers")
+
+# %%
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_NONE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+}
+
+
+def llm(transcript: str) -> str:
+    input_text = "Give me the last statistics for the match between the marvelous Celtics and Bulls"
+    llm_model = GenerativeModel("gemini-pro")
+    fc_chat = llm_model.start_chat()
+    res = fc_chat.send_message(
+        input_text,
+        tools=[all_tools],
+        safety_settings=safety_settings
+    )
+    text = gem_utils.get_text(res)
+    if not text:
+        name = gem_utils.get_function_name(res)
+        args = gem_utils.get_function_args(res)
+        print(f"AGENT: FUNCTION CALL: {name}({args})\n")
+        if name == "get_stats_funct":
+            stats = get_stats_funct(args["team_1"], args["team_2"])
+        else:
+            stats = "There is no information yet."
+
+        llm_res = fc_chat.send_message(
+            f"""You are an expert in NBA analysis, oftentimes funny!, these are your instructions:
+            - Your first task is to Extract important highlights from the `Transcript`.
+            - The following `Statistics` are from the last 5 games between both teams, your second task is to analyze 
+            and enrich your comments to be creative.
+            - With all the information create a very accurate and detailed analysis of the game.
+            - Do not fake your answer, you have many elements to create a very factual description.
+            
+            Transcript:
+            {transcript}
+            
+            Statistics:
+            {stats}
+            
+            Output:
+            """,
+            generation_config={"max_output_tokens": 8000}
+        )
+        print(llm_res.text)
+
+        return llm_res.text
+
+
+# %%
+# Exploring Transcriptions
+audio_trans = [blob.name for blob in bucket.list_blobs(prefix="transcription")]
+str_json = bucket.blob(audio_trans[0]).download_as_text()
+dict_res = json.loads(str_json)
+_text = []
+for result in dict_res["results"]:
+    for i in result["alternatives"]:
+        _text.append(i["transcript"])
+r = llm(" ".join(_text))
+st.info("Transcription:")
+st.write(" ".join(_text))
+st.info(r)
---
+++ preprocessing.py
@@ -0,0 +1,89 @@
+# %%
+# Libraries Definitions
+import re
+import json
+from moviepy.editor import *
+from google.cloud import storage
+from google.cloud.speech_v2 import SpeechClient
+from google.cloud.storage import transfer_manager
+from google.cloud.speech_v2.types import cloud_speech
+
+project_id = "vtxdemos"
+bucket_id = "vtxdemos-nbademos"
+video_dir_path = "videos"
+audio_dir_path = "audio"
+gcs_audio = "gs://vtxdemos-nbademos/audio"
+gcs_output_path_speech_to_text = "gs://vtxdemos-nbademos/transcription"
+
+client = storage.Client(project_id)
+bucket = client.bucket(bucket_id)
+names = [f"gs://{bucket_id}/" + blob.name for blob in bucket.list_blobs()]
+blob = bucket.blob("clippers_thunders.mp4")
+
+client = SpeechClient()
+# noinspection PyTypeChecker
+config = cloud_speech.RecognitionConfig(
+    auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
+    language_codes=["en-US"],
+    model="long",
+)
+
+# %%
+# Download Videos Locally
+blob_names = [blob.name for blob in bucket.list_blobs()]
+results = transfer_manager.download_many_to_path(
+    bucket, blob_names, destination_directory=video_dir_path, max_workers=8
+)
+
+# %%
+# From Video to Audio
+for n, blob in enumerate(bucket.list_blobs(prefix="videos")):
+    if n == 0:
+        continue
+    video = blob.name
+    vide_name = video.split("/")[-1]
+    print(video)
+    vid = VideoFileClip(f"{video_dir_path}/{vide_name}")
+    vid.audio.write_audiofile(f"{audio_dir_path}/{vide_name.split('.')[-2]}.mp3")
+
+# %%
+# Upload Audio to Google Cloud Storage
+mp3_pattern = re.compile(r'\.mp3$', re.IGNORECASE)
+audio_files = [audio_dir_path + "/" + filename for filename in os.listdir(audio_dir_path) if
+               mp3_pattern.search(filename)]
+transfer_manager.upload_many_from_filenames(bucket=bucket, filenames=audio_files)
+
+# %%
+# Transcriptions using Chirp
+gcs_uris = [f"gs://{bucket_id}/" + blob.name for blob in bucket.list_blobs(prefix="audio")]
+
+files = [
+    cloud_speech.BatchRecognizeFileMetadata(uri=uri)
+    for uri in gcs_uris
+]
+# noinspection PyTypeChecker
+request = cloud_speech.BatchRecognizeRequest(
+    recognizer=f"projects/{project_id}/locations/global/recognizers/_",
+    config=config,
+    files=files,
+    recognition_output_config=cloud_speech.RecognitionOutputConfig(
+        gcs_output_config=cloud_speech.GcsOutputConfig(
+            uri=gcs_output_path_speech_to_text,
+        ),
+    ),
+)
+operation = client.batch_recognize(request=request)
+print("Waiting for operation to complete...")
+response = operation.result(timeout=1000)
+
+# %%
+# Exploring Transcriptions
+audio_trans = [blob.name for blob in bucket.list_blobs(prefix="transcription")]
+
+str_json = bucket.blob(audio_trans[0]).download_as_text()
+
+dict_res = json.loads(str_json)
+
+for result in dict_res["results"]:
+    for i in result["alternatives"]:
+        print(i["transcript"])
